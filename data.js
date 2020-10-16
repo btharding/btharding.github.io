d7ea41d424a6e3ea68d1cd45fc2b70cfbe0a7b8f
btharding <btharding2014@hotmail.com>
2020-10-16 15:09:59 +0100

    Updated CV

--- data.js
+++ data.js
@@ -4,8 +4,16 @@ const skills = [
   'level': '5'
   },
   {
+  'skill': 'Javascript',
+  'level': '5'
+  },
+  {
   'skill': 'CSS',
   'level': '5'
+  },
+  {
+  'skill': 'React',
+  'level': '4'
   },
   {
   'skill': 'Java',
@@ -16,11 +24,11 @@ const skills = [
   'level': '4'
   },
   {
-  'skill': 'Javascript',
+  'skill': 'PHP',
   'level': '4'
   },
   {
-  'skill': 'React',
+  'skill': 'SQL',
   'level': '4'
   },
   {
@@ -28,12 +36,12 @@ const skills = [
   'level': '3'
   },
   {
-  'skill': 'SQL',
-  'level': '3'
+  'skill': 'C',
+  'level': '2'
   },
   {
-  'skill': 'PHP',
-  'level': '3'
+  'skill': 'Haskell',
+  'level': '2'
   }
 ];
 
@@ -51,7 +59,7 @@ const experiences =[
   'description':'<ul><li>Worked as a team to lead junior coaches in delivering gymnastics sessions to large groups</li><li>Planned and organised sessions and workshops</li><li>Delegated coaching responsibilities to junior coaches</li></ul>'
   },
   {
-  'date': 'Dec 2017 - Present',
+  'date': 'Dec 2017 - Oct 2020',
   'experience': 'Customer Assistant',
   'location': 'McDonalds',
   'description': '<ul><li>Customer Facing - Served customers, took payments, and maintained high levels of cleanliness</li><li>Kitchen - Prepped food in line with food safety guidelines and managed stock levels based on customer frequency</li></ul>'
@@ -62,16 +70,21 @@ const experiences =[
   'location': 'MyTutor',
   'description': '<ul><li>Tutoring GCSE and A-Level Maths and Computer Science as well as Python programming</li><li>Write lesson plans and deliver one-on-one online lessons</li><li>Enhanced DBS check as part of the MyTutor Schools Program</li></ul>'
   },
-
+  {
+  'date': 'Sept 2020 - Present',
+  'experience': 'Auditor',
+  'location': 'Serve Legal',
+  'description': '<ul><li>Responsible for ensuring shops adhere to age restriction laws.</li><li>Write detailed reports of staff actions and my overall experience.</li><li>Planned routes and timings between many audits in a single day.</li></ul>'
+  }
 ];
 
 
 const projects = [
   {
-  'title': 'Vegan Carbon Calculator',
-  'github':'https://github.com/btharding/Vegan-Carbon-Calculator',
-  'link': 'https://btharding.github.io/btharding/',
-  'description': 'A work in progress. This was initially made for the lockdoneathon hackathon in just 2 days.<br/>This web app uses the spoonacular API to fetch recipes, then approximates the carbon footprint of the dish and the carbon footprint of a vegan version of the dish. All of the front end is done using ReactJS, and the logic using Javascript.<br/>Next steps can be seen in the README of the github repo.',
+  'title': 'Veganalysis',
+  'github':'https://github.com/btharding/Veganalysis',
+  'link': 'https://btharding.github.io/Veganalysis/',
+  'description': 'Based on a web-app made for the Lockdoneathon Hackathon in just 2 days, this web-app highlights the ecological impact of veganism by comparing the carbon footprint of recipes with their vegan counterparts.<br/>The front end is built off React, with a backend based on the <a href = "https://spoonacular.com/food-api" target = "blank">Spoonacular recipe searching API</a> and a novel database of vegan substitutions and carbon footprints.<br />I am in the process of extending functionality, having recently updated styling, refactored code and made it all more efficient, as well as implementing deeplinking using React routers.<br/>I am also developing a PHP-powered API to query the substitutions and carbon footprint database.<br/>Next steps can be viewed in the README of the github repo.',
   'lang': ['ReactJS', 'Javascript', 'HTML', 'CSS']
   },
   {
